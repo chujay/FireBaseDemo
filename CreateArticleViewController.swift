348d0a5eec4a4b593464993f3ae7712799eaeaef
Luke Wu <wuchiwei17@gmail.com>
2017-03-14 16:27:05 +0800

    Init commit, Create Article.storyboard and AddArticleViewController.swift

---
+++ CreateArticleViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseTutorial
+//
+//  Created by 伍智瑋 on 2017/3/14.
+//  Copyright © 2017年 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class CreateArticleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
